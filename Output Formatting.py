dff3567654229ee89d99a4c161271e69a7dee963
aakankshaaa07 <55917789+aakankshaaa07@users.noreply.github.com>
2020-06-19 19:45:42 +0530

    hello
    This is how you express numbers in Python.

---
+++ Output Formatting.py
@@ -0,0 +1,14 @@
+#printing an integer value //syntax: "%[width]d"
+print("%8d" %(131072))
+
+#printing a float value //synatx: "%[width].[precision]f"
+print("%7.2f" %(2.048))
+
+#printing a number in it's octal form //syntax: "%[width].[number_of_digits]o"
+print("%5.3o" %(25))
+
+#printing a number in it's hexadecimal form //syntax: "%[width].[number_of_digits]x"
+print("%5.3x" %(25))
+
+#printing a number in it's exponential form //syntax: "%[width].[number_of_digits]e"
+print("%5.4e" %(131.072))
